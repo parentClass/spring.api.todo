0141069c8eb8cbf39cea020cdbfd3dd64624b04a
parentclass <dimalantadaniel@gmail.com>
2021-07-15 20:37:35 +0800

    Initial commit

---
+++ src/test/kotlin/com/parentclass/springportfolio/TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.parentclass.springportfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
